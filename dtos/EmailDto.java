a6f9d4474fff46d9c9dc2039092fc175fc07cb65
Perkles <victor.coelho@discente.ufma.br>
2023-08-03 17:41:55 -0300

    repository, service and controller

--- dtos/EmailDto.java
+++ dtos/EmailDto.java
@@ -1,5 +1,29 @@
 package dtos;
 
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class EmailDto {
+	
+	@NotBlank  //validação para obrigar preenchimento
+	private String owernRef;
+	
+	@NotBlank
+	@Email  //verifica se o email existe
+	private String emailFrom;
+	
+	@NotBlank
+	@Email
+	private String emailTo;
+	
+	@NotBlank
+	private String subject;
+	
+	@NotBlank
+	private String text;
 
 }
