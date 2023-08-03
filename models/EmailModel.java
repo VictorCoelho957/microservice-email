24b90e1a7f33e88faf756ccdf30e08b55e25a76c
Perkles <victor.coelho@discente.ufma.br>
2023-08-03 17:10:18 -0300

    class model email and Status Email

--- models/EmailModel.java
+++ models/EmailModel.java
@@ -1,5 +1,56 @@
 package models;
 
-public class EmailModel {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+//classe com os dados
+
+@Data
+@Entity
+@Table(name="TB_EMAIL")
+public class EmailModel implements 	Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long emailId;
+	
+	private String owernRef;
+	
+	private String emailFrom;
+	
+	private String emailTo;
+	
+	private String subject;
+	
+	@Column(columnDefinition="TEXT")
+	private String text;
+	
+	private  LocalDateTime sendDateEmail;
+	
+	//tipo enumerado
+	private statusEmail statusEmail;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
